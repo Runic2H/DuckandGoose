c45d966d0111b565aa45c1097f79d7f20215ff91
EggyBro <tan.ek.hern@gmail.com>
2023-04-03 12:47:57 +0800

    holding files for implementation of charged attack sounds

---
+++ ExoEngine/OnChargeAttack_2.cpp
@@ -0,0 +1,151 @@
+/*!*************************************************************************
+****
+\file OnChargeAttack_2.cpp
+\author Elton Teo Zhe Wei
+\par DP email: e.teo@digipen.edu
+\par Course: CSD2450
+\par Section: a
+\par Assignment GAM200
+\date 24/2/2022
+\brief	1st Charge attacking state for player
+
+Copyright (C) 20xx DigiPen Institute of Technology. Reproduction or disclosure of this file or its contents
+without the prior written consent of DigiPen Institute of Technology is prohibited.
+****************************************************************************
+***/
+#include "empch.h"
+#include "OnChargeAttack_2.h"
+#include "OnIdle.h"
+#include "OnBlock.h"
+#include "OnDamaged.h"
+#include "OnAttack_2.h"
+#include "ExoEngine/Timer/Time.h"
+
+namespace EM
+{
+	OnChargeAttack_2::OnChargeAttack_2(StateMachine* stateMachine) : mTimer{ 0.0f }, mDuration{ Timer::GetInstance().GetGlobalDT() }, mChargeIndex{ 0 } { UNREFERENCED_PARAMETER(stateMachine); }
+
+	IStates* OnChargeAttack_2::HandleInput(StateMachine* stateMachine, const int& key)
+	{
+		UNREFERENCED_PARAMETER(stateMachine);
+		UNREFERENCED_PARAMETER(key);
+		return nullptr;
+	}
+	/*!*************************************************************************
+		Enter state for Player Attack 1
+	****************************************************************************/
+	void OnChargeAttack_2::OnEnter(StateMachine* stateMachine)
+	{
+		if (p_ecs.HaveComponent<PlayerAttributes>(stateMachine->GetEntityID())) {
+			p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mChargeTimer = 0.5f;
+			p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mCooldownTimer = 0.3f;
+		}
+
+		p_ecs.GetComponent<Sprite>(stateMachine->GetEntityID()).SetTexture("Normal_Attack_Swing2");
+		p_ecs.GetComponent<Sprite>(stateMachine->GetEntityID()).is_Animated = false;
+	}
+
+	/*!*************************************************************************
+		Update state for Player Attack 1
+	****************************************************************************/
+	void OnChargeAttack_2::OnUpdate(StateMachine* stateMachine, float Frametime)
+	{
+		if (p_ecs.HaveComponent<PlayerAttributes>(stateMachine->GetEntityID())) {
+			p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mBlockCoolDown <= 0.0f ? 0.0f : p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mBlockCoolDown -= Frametime;
+			p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mDashCoolDown <= 0.0f ? 0.0f : p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mDashCoolDown -= Frametime;
+			p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mDamageCoolDown <= 0.0f ? 0.0f : p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mDamageCoolDown -= Frametime;
+		}
+		if (p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mIsDamaged)
+		{
+			stateMachine->ChangeState(new OnDamaged(stateMachine));
+		}
+		if (p_Input->MouseHold(GLFW_MOUSE_BUTTON_LEFT) && !p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mIsChargeAttack)
+		{
+			p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mChargeTimer <= 0.0f ? 0.0f : p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mChargeTimer -= Frametime;
+			if (p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mChargeTimer < 0.3f)
+			{
+				p_ecs.GetComponent<Sprite>(stateMachine->GetEntityID()).SetTexture("CA2");
+				p_ecs.GetComponent<Sprite>(stateMachine->GetEntityID()).is_Animated = true;
+				
+				//play charging sound
+				if (p_ecs.HaveComponent<Audio>(stateMachine->GetEntityID()) && (p_ecs.GetComponent<Audio>(stateMachine->GetEntityID()).GetSize() > 17))
+				{
+					p_ecs.GetComponent<Audio>(stateMachine->GetEntityID())[17].should_play = true; //charge atk start
+				}
+			}
+			if (p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mChargeTimer <= 0.0f)
+			{
+				p_ecs.GetComponent<Sprite>(stateMachine->GetEntityID()).is_Animated = false;
+			
+				mChargeIndex = p_ecs.GetComponent<Sprite>(stateMachine->GetEntityID()).GetIndex().x - 4;
+				mTimer += Frametime;
+				if (mTimer >= mDuration) {
+					mTimer = 0.f;
+					std::swap(mChargeIndex, p_ecs.GetComponent<Sprite>(stateMachine->GetEntityID()).GetIndex().x);
+					std::swap(p_ecs.GetComponent<Sprite>(stateMachine->GetEntityID()).GetIndex().x, mChargeIndex);
+					//play charged sound
+					if (p_ecs.HaveComponent<Audio>(stateMachine->GetEntityID()) && (p_ecs.GetComponent<Audio>(stateMachine->GetEntityID()).GetSize() > 18))
+					{
+						p_ecs.GetComponent<Audio>(stateMachine->GetEntityID())[18].should_play = true; // looped charging
+					}
+				}
+			}
+		}
+		else if (p_Input->MouseIsReleased(GLFW_MOUSE_BUTTON_LEFT) && p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mChargeTimer <= 0.0f)
+		{
+			auto& transform = p_ecs.GetComponent<Transform>(stateMachine->GetEntityID());
+			auto& rigidbody = p_ecs.GetComponent<RigidBody>(stateMachine->GetEntityID());
+			vec2D dir = rigidbody.GetDir();
+			dir = dir * 20.0f;
+			rigidbody.SetVel(p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mPhys.accelent(rigidbody.GetVel(), dir, Frametime));
+			vec2D nextPos = (transform.GetPos() + rigidbody.GetVel());
+			rigidbody.SetNextPos(nextPos);
+			p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mIsChargeAttack = true;
+			p_ecs.GetComponent<Sprite>(stateMachine->GetEntityID()).is_Animated = true;
+			if (p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mCooldownTimer <= 0.15f)
+			{
+				p_ecs.GetComponent<Collider>(stateMachine->GetEntityID()).GetCollisionArray()[1].is_Alive = true;
+				//play attack swing
+				if (p_ecs.HaveComponent<Audio>(stateMachine->GetEntityID()) && (p_ecs.GetComponent<Audio>(stateMachine->GetEntityID()).GetSize() > 9))
+				{
+					p_ecs.GetComponent<Audio>(stateMachine->GetEntityID())[9].should_play = true;
+				}
+			}
+			if (p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mHitStopTimer <= 0.0f)
+			{
+				p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mCooldownTimer <= 0.0f ? 0.0f : p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mCooldownTimer -= Frametime;
+				p_ecs.GetComponent<Sprite>(stateMachine->GetEntityID()).is_Animated = true;
+				if (p_ecs.HaveComponent<PlayerAttributes>(stateMachine->GetEntityID())) {
+					if (p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mIsDamaged)
+					{
+						stateMachine->ChangeState(new OnDamaged(stateMachine));
+					}
+					if (p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mCooldownTimer <= 0.0f)
+					{
+						stateMachine->ChangeState(new OnIdle(stateMachine));
+					}
+				}
+			}
+			else
+			{
+				p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mHitStopTimer <= 0.0f ? 0.0f : p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mHitStopTimer -= Frametime;
+				p_ecs.GetComponent<Sprite>(stateMachine->GetEntityID()).is_Animated = false;
+			}
+		}
+		else if (p_Input->MouseIsReleased(GLFW_MOUSE_BUTTON_LEFT))
+		{
+			stateMachine->ChangeState(new OnAttack_2(stateMachine));
+		}
+	}
+	/*!*************************************************************************
+		Exit state for Player Attack 1
+	****************************************************************************/
+	void OnChargeAttack_2::OnExit(StateMachine* stateMachine)
+	{
+		p_ecs.GetComponent<Collider>(stateMachine->GetEntityID()).GetCollisionArray()[1].is_Alive = false;
+		p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mIsChargeAttack = false;
+		p_ecs.GetComponent<PlayerAttributes>(stateMachine->GetEntityID()).mHitStopTimer = 0.0f;
+		p_ecs.GetComponent<Sprite>(stateMachine->GetEntityID()).GetIndex().x = 0;
+		delete this;
+	}
+}
